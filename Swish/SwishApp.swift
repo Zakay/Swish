f561a7c40955faa059b1a53264130c379cb8690e
Zakay Danial <zakay@danial.se>
2025-06-12 00:09:54 +0200

    Initial Commit

---
+++ Swish/SwishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwishApp.swift
+//  Swish
+//
+//  Created by Zakay Danial on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
